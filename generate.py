85e26e7d8e4ce332c2d332e0cf733b57f64086c5
SvetlanaShir <71212614+SvetlanaShir@users.noreply.github.com>
2020-09-13 21:55:51 +0300

    Add files via upload

--- generate.py
+++ generate.py
@@ -11,8 +11,9 @@ model = args.model
 length = int(args.length)
 seed = args.seed
 if seed == None:
-    seed='случайное слово'
-    
+    seed = 'случайное слово'
+
+
 class Code:
     def generate(model, length, seed):
         with open(model, 'rb') as f:
@@ -22,25 +23,27 @@ class Code:
                 word = random.choice(dict_new['начало предложения'])
             else:
                 word = seed.lower()
-            print (word[0].upper(), end='')
-            for k in range(len(word)-1):
-                print (word[k+1], end='')
+            print(word[0].upper(), end='')
+            for k in range(len(word) - 1):
+                print(word[k+1], end='')
             sentence_length = random.randint(3, 12)
             for i in range(20):
                 if word in dict_new:
                     word = random.choice(dict_new[word])
-                    if i > sentence_length  and word in dict_new['конец предложения']:
-                        print ('', word, end='. ')
-                        seed='случайное слово'
+                    if i > sentence_length and word in dict_new['конец предложения']:
+                        print('', word, end='. ')
+                        seed = 'случайное слово'
                         break
                     else:
-                        print ('', word, end='')
+                        print('', word, end='')
                 else: 
                     print('. ', end='')
-                    seed='случайное слово'
+                    seed = 'случайное слово'
                     break
                 if i == 19:
                     print('. ', end='')
-                    seed='случайное слово'
+                    seed = 'случайное слово'
                     break
+
+
 Code.generate(model, length, seed)

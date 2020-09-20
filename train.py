1b11746aa74144b612fc2a6cfdae7943c56eed63
SvetlanaShir <71212614+SvetlanaShir@users.noreply.github.com>
2020-09-20 12:15:01 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -11,12 +11,14 @@ model = args.model
 
 
 class Code:
-    ABC = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    ABC = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя-'
+    CAPS_ABC= 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     punctuation = ',.!?: '
     my_dict = dict()
     my_dict['конец предложения'] = []
     my_dict['начало предложения'] = []
-
+    my_dict['слова с большой буквы'] = []
+    small_words={'слово'}
     def fit(dir_name, model):
         folder = os.listdir(dir_name)
         for file in folder:
@@ -51,12 +53,42 @@ class Code:
                         else:
                             word = ''
                             previous_word = ''
-                            begin = 0                            
+                            if not symbol ==' ':
+                                begin = 0
+            #часть кода дальше нужна, чтобы слова с большой буквы (в основном имена) были написаны с большой буквы. Эта часть не подходит для стихов, например.
+            with open(path, "r", encoding='utf-8') as inf:
+                for line in inf:
+                    line = line.strip()
+                    line += ' '
+                    word = ''
+                    for symbol in line:
+                        if symbol in Code.ABC or symbol in Code.CAPS_ABC:
+                            word += symbol
+                        elif symbol in Code.punctuation:
+                            if word == word.lower():
+                                Code.small_words.add(word)                           
+                            word = ''
+                        else: word = ''
+        for file in folder:
+            path = dir_name + '/' + file
+            with open(path, "r", encoding='utf-8') as inf:
+                for line in inf:
+                    line = line.strip()
+                    line += ' '
+                    word = ''
+                    for symbol in line:
+                        if symbol in Code.ABC or symbol in Code.CAPS_ABC:
+                            word += symbol
+                        elif symbol in Code.punctuation and not word == '':
+                            if word[0] in Code.CAPS_ABC and not word.lower() in Code.small_words:
+                                Code.my_dict['слова с большой буквы'].append(word.lower()) 
+                            word = ''
+                        else: word = '' 
+        
         with open(model, 'wb') as f:
             pickle.dump(Code.my_dict, f)
-
+        
     def print():
         print(Code.my_dict)
-
 
 Code.fit(dir_name, model)
